78a8a1d5fb4839cff8e2f836519b0383817333ef
votiveamit13 <votiveamit.web@gmail.com>
2025-05-27 18:05:12 +0530

    userdashboard

---
+++ src/components/userdashboard/UserHeader.jsx
@@ -0,0 +1,160 @@
+"use client";
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function UserHeader({user}) {
+  const [collapsed, setCollapsed] = useState(false);
+
+  const toggleSidebar = () => {
+    const sidebar = document.getElementById("sidebar");
+    if (sidebar) {
+      sidebar.classList.toggle("collapsed");
+    }
+    setCollapsed(!collapsed);
+  };
+  return (
+    <>
+      <nav className="navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
+        <div className="text-center navbar-brand-wrapper d-flex align-items-center justify-content-start">
+          <a className="navbar-brand" href="#">
+            <img
+              src="assets/images/favicon.png"
+              alt="favicon logo"
+              className="navbar-brand-img d-lg-none"
+            />
+            <div className="d-none d-lg-block">
+              {collapsed ? (
+                <img
+                  src="assets/images/favicon.png"
+                  alt="collapsed logo"
+                  className="navbar-brand-img"
+                />
+              ) : (
+                <img
+                  src="assets/images/logo.png"
+                  alt="full logo"
+                  className="navbar-brand-img"
+                />
+              )}
+            </div>
+          </a>
+
+          <button
+            className="btn btn-link d-none d-lg-block ms-3"
+            onClick={toggleSidebar}
+          >
+            <i className="bi bi-list fs-3"></i>
+          </button>
+        </div>
+
+        <div className="navbar-menu-wrapper d-flex align-items-center justify-content-end">
+          <button
+            className="navbar-toggler navbar-toggler align-self-center"
+            type="button"
+            data-toggle="minimize"
+          >
+            <span className="icon-menu"></span>
+          </button>
+          <ul className="navbar-nav navbar-nav-right">
+            <li className="nav-item dropdown">
+              <a
+                className="nav-link count-indicator dropdown-toggle"
+                id="notificationDropdown"
+                href="#"
+                data-bs-toggle="dropdown"
+              >
+                <i className="bi bi-bell"></i>
+                <span className="count"></span>
+              </a>
+              <div
+                className="dropdown-menu dropdown-menu-right navbar-dropdown preview-list"
+                aria-labelledby="notificationDropdown"
+              >
+                <p className="mb-0 font-weight-normal float-left dropdown-header">
+                  Notifications
+                </p>
+                <a className="dropdown-item preview-item">
+                  <div className="preview-thumbnail">
+                    <div className="preview-icon bg-success">
+                      <i className="bi bi-info-circle mx-0"></i>
+                    </div>
+                  </div>
+                  <div className="preview-item-content">
+                    <h6 className="preview-subject font-weight-normal">
+                      Application Error
+                    </h6>
+                    <p className="font-weight-light small-text mb-0 text-muted">
+                      {" "}
+                      Just now{" "}
+                    </p>
+                  </div>
+                </a>
+                <a className="dropdown-item preview-item">
+                  <div className="preview-thumbnail">
+                    <div className="preview-icon bg-warning">
+                      <i className="bi bi-gear mx-0"></i>
+                    </div>
+                  </div>
+                  <div className="preview-item-content">
+                    <h6 className="preview-subject font-weight-normal">
+                      Settings
+                    </h6>
+                    <p className="font-weight-light small-text mb-0 text-muted">
+                      {" "}
+                      Private message{" "}
+                    </p>
+                  </div>
+                </a>
+                <a className="dropdown-item preview-item">
+                  <div className="preview-thumbnail">
+                    <div className="preview-icon bg-info">
+                      <i className="bi bi-person mx-0"></i>
+                    </div>
+                  </div>
+                  <div className="preview-item-content">
+                    <h6 className="preview-subject font-weight-normal">
+                      New user registration
+                    </h6>
+                    <p className="font-weight-light small-text mb-0 text-muted">
+                      {" "}
+                      2 days ago{" "}
+                    </p>
+                  </div>
+                </a>
+              </div>
+            </li>
+            <li className="nav-item nav-profile dropdown">
+              <a
+                className="nav-link dropdown-toggle"
+                href="#"
+                data-bs-toggle="dropdown"
+                id="profileDropdown"
+              >
+                <img src="assets/images/faces/face-img.png" alt="profile" />
+                <p className="top-name-add">{user?.first_name}</p>
+              </a>
+              <div
+                className="dropdown-menu dropdown-menu-right navbar-dropdown"
+                aria-labelledby="profileDropdown"
+              >
+                <a className="dropdown-item">
+                  <i className="bi bi-gear mx-0"></i>&nbsp; Settings{" "}
+                </a>
+                <a className="dropdown-item">
+                  <i className="bi bi-power text-primary"></i>&nbsp;Logout{" "}
+                </a>
+              </div>
+            </li>
+          </ul>
+          <button
+            className="navbar-toggler navbar-toggler-right d-lg-none align-self-center"
+            type="button"
+            data-bs-toggle="offcanvas"
+          >
+            <i className="bi bi-list fs-2"></i>
+          </button>
+        </div>
+      </nav>
+    </>
+  );
+}
