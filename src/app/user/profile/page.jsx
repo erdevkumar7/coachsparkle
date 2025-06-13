c0c497a7ce21f368d46cc74f6e87909f93fb0aac
erdevkumar7 <erdevkumar7@gmail.com>
2025-06-13 17:43:04 +0530

    add usersiderbar component

--- src/app/user/profile/page.jsx
+++ src/app/user/profile/page.jsx
@@ -3,147 +3,145 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import "../_styles/dashboard.css";
 import "../_styles/profile.css";
+import axios from "axios";
+import UserSideBarComp from "../_user_components/UserSideBar";
+import { FRONTEND_BASE_URL } from "@/config/url_config";
 
 
 export default function Profile() {
     const router = useRouter();
-    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
     const [user, setUser] = useState(null);
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        email: '',
+        contactNumber: '',
+        countryId: '',
+
+        professionalTitle: '',
+        detailedBio: '',
+        shortBio: '',
+        userTimezone: '',
+        gender: '',
+        stateId: '',
+        cityId: '',
+
+        coachingCategory: '',
+        deliveryMode: '',
+        freeTrialSession: '',
+        isVolunteeredCoach: '',
+        volunteerCoaching: '',
+        videoLink: '',
+        experience: '',
+        price: '',
+        websiteLink: '',
+        facebookLink: '',
+        instaLink: '',
+        linkdinLink: '',
+        bookingLink: '',
+        objective: '',
+        coachType: '',
+        coachSubtype: '',
+        profileImage: '',
+        serviceNames: [],
+        languageNames: [],
+    });
+
 
     useEffect(() => {
-        const token = localStorage.getItem("token");
-        const userData = localStorage.getItem("user");
-
-        if (!token) {
-            router.push("/login");
-        }
-
-        if (token && userData) {
-            setIsLoggedIn(true);
-            setUser(JSON.parse(userData));
-        } else {
-            setIsLoggedIn(false);
-            setUser(null);
-        }
+        getUserData();
     }, []);
 
+    const getUserData = async () => {
+        const token = localStorage.getItem("token");
+        if (!token) {
+            setUser(null);
+            router.push("/login");
+            return;
+        }
+
+        try {
+            const response = await axios({
+                url: `${apiUrl}/getuserprofile`,
+                headers: {
+                    "accept": "application/json",
+                    "authorization": `Bearer ${token}`,
+                },
+                method: 'POST',
+            })
+
+            if (response.data.success) {
+                const data = response.data.data;
+                setUser(data);
+
+                setFormData({
+                    firstName: data.first_name || '',
+                    lastName: data.last_name || '',
+                    email: data.email || '',
+                    contactNumber: data.contact_number || '',
+                    countryId: data.country_id || '',
+
+                    professionalTitle: data.professional_title || '',
+                    detailedBio: data.detailed_bio || '',
+                    shortBio: data.short_bio || '',
+                    userTimezone: data.user_timezone || '',
+                    gender: data.gender || '',
+                    stateId: data.state_id || '',
+                    cityId: data.city_id || '',
+
+                    coachingCategory: data.coaching_category || '',
+                    deliveryMode: data.delivery_mode || '',
+                    freeTrialSession: data.free_trial_session || '',
+                    isVolunteeredCoach: data.is_volunteered_coach || '',
+                    volunteerCoaching: data.volunteer_coaching || '',
+                    videoLink: data.video_link || '',
+                    experience: data.experience || '',
+                    price: data.price || '',
+                    websiteLink: data.website_link || '',
+                    facebookLink: data.facebook_link || '',
+                    instaLink: data.insta_link || '',
+                    linkdinLink: data.linkdin_link || '',
+                    bookingLink: data.booking_link || '',
+                    objective: data.objective || '',
+                    coachType: data.coach_type || '',
+                    coachSubtype: data.coach_subtype || '',
+                    profileImage: data.profile_image || '',
+                    serviceNames: data.service_names || [],
+                    languageNames: data.language_names || [],
+                });
+            } else {
+                handleLogout();
+            }
+        } catch (error) {
+            console.error("Authentication failed:", error);
+            handleLogout();
+        }
+    };
+
+    // Handle form field changes
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+
+        setFormData((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
+    const handleLogout = () => {
+        localStorage.removeItem("token");
+        localStorage.removeItem("user");
+        setUser(null);
+        router.push("/login");
+    };
+
+    // console.log(user, 'user')
+  
     return (
         <>
             <div className="container-fluid page-body-wrapper">
-                <nav className="sidebar sidebar-offcanvas" id="sidebar">
-                    <ul className="nav">
-                        <li className="nav-item">
-                            <a className="nav-link" href="#" data-bs-toggle="collapse" aria-expanded="false" aria-controls="ui-basic">
-                                <div>
-                                    <i className="bi bi-grid-3x3-gap-fill"></i>
-                                    <span className="menu-title">Overview</span>
-                                </div>
-                            </a>
-                        </li>
-
-                        <li className="nav-item explore-tab">
-                            <a className="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                                <div>
-                                    <i className="bi bi-search"></i>
-                                    <span className="menu-title">Explore Coaches</span>
-                                </div>
-                            </a>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                                <div>
-                                    <i className="bi bi-duffle"></i>
-                                    <span className="menu-title">Coaching Activities</span>
-                                </div>
-                            </a>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                                <div>
-                                    <i className="bi bi-heart"></i>
-                                    <span className="menu-title">Favourite Coach</span>
-                                </div>
-                            </a>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                                <div>
-                                    <i className="bi bi-chat-dots"></i>
-                                    <span className="menu-title">Message</span>
-                                </div>
-                            </a>
-                        </li>
-
-                        <li className="nav-item">
-                            <a className="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                                <div>
-                                    <i className="bi bi-calendar2-week"></i>
-                                    <span className="menu-title">Booking</span>
-                                </div>
-                            </a>
-                        </li>
-
-
-
-                        <li className="nav-item">
-                            <a className="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                                <div>
-                                    <i className="bi bi-star"></i>
-                                    <span className="menu-title">Reviews</span>
-                                </div>
-                            </a>
-                        </li>
-
-
-
-
-                        <li className="nav-item profile-tab">
-                            <a className="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                                <div>
-                                    <i className="bi bi-person"></i>
-                                    <span className="menu-title">Profile</span>
-                                </div>
-                            </a>
-                        </li>
-
-
-
-
-                        <li className="nav-item">
-                            <a className="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                                <div>
-                                    <i className="bi bi-gear"></i>
-                                    <span className="menu-title">Account Settings</span>
-                                </div>
-                            </a>
-                        </li>
-
-
-
-                        <li className="nav-item">
-                            <a className="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                                <div>
-                                    <i className="bi bi-headset"></i>
-                                    <span className="menu-title">FAQs and Support</span>
-                                </div>
-                            </a>
-                        </li>
-
-
-                        <li className="nav-item sign-out">
-                            <a className="nav-link" data-bs-toggle="collapse" href="#ui-basic" aria-expanded="false" aria-controls="ui-basic">
-                                <div>
-                                    <i className="fa fa-sign-out" aria-hidden="true"></i>
-                                    <span className="menu-title">Sign Out</span>
-                                </div>
-                            </a>
-                        </li>
-
-                    </ul>
-                </nav>
+                <UserSideBarComp />        
 
                 <div className="main-panel">
                     <div className="content-wrapper">
@@ -164,7 +162,7 @@ export default function Profile() {
                             <div className="card">
                                 <h3 className="text-your-name">Your Profile</h3>
                                 <div className="upload-photo-add">
-                                    <img src="/coachsparkle/assets/images/top-nav.png" alt="profile" />
+                                    <img src={`${FRONTEND_BASE_URL}/assets/images/faces/face-img.png`} alt="profile"  />
                                     <div className="upload-btn">
                                         <a href="#"><i className="bi bi-upload"></i> Upload photo</a>
 
@@ -176,12 +174,26 @@ export default function Profile() {
                                         <div className="form-row">
                                             <div className="form-group">
                                                 <label htmlFor="firstName">First Name*</label>
-                                                <input type="text" id="firstName" name="firstName" required />
+                                                <input
+                                                    type="text"
+                                                    id="firstName"
+                                                    name="firstName"
+                                                    required
+                                                    value={formData.firstName}
+                                                    onChange={handleChange}
+                                                />
                                             </div>
 
                                             <div className="form-group">
                                                 <label htmlFor="lastName">Last Name</label>
-                                                <input type="text" id="lastName" name="lastName" />
+                                                <input
+                                                    type="text"
+                                                    id="lastName"
+                                                    name="lastName"
+                                                    required
+                                                    value={formData.lastName}
+                                                    onChange={handleChange}
+                                                />
                                             </div>
 
                                             <div className="form-group">
@@ -196,7 +208,14 @@ export default function Profile() {
 
                                             <div className="form-group">
                                                 <label htmlFor="email">Email*</label>
-                                                <input type="email" id="email" name="email" required />
+                                                <input
+                                                    type="email"
+                                                    id="email"
+                                                    name="email"
+                                                    required
+                                                    value={formData.email}
+                                                    onChange={handleChange}
+                                                />
                                             </div>
 
                                             <div className="form-group">
@@ -204,11 +223,6 @@ export default function Profile() {
                                                 <select id="location" name="location">
                                                     <option value="">Select Location</option>
                                                     <option value="new-york">New York</option>
-                                                    <option value="los-angeles">Los Angeles</option>
-                                                    <option value="chicago">Chicago</option>
-                                                    <option value="houston">Houston</option>
-                                                    <option value="san-francisco">San Francisco</option>
-                                                    <option value="other">Other</option>
                                                 </select>
                                             </div>
 
@@ -292,7 +306,10 @@ export default function Profile() {
 
                                         <div className="form-group full-width">
                                             <label htmlFor="bio">Short Bio (Optional)</label>
-                                            <textarea id="bio" name="bio" rows="3" placeholder="Write a short bio..."></textarea>
+                                            <textarea
+                                                id="bio" name="bio" rows="3" placeholder="Write a short bio..."
+                                                onChange={handleChange}
+                                                value={formData.shortBio}></textarea>
                                         </div>
 
                                         <div className="form-group check-box">
@@ -305,18 +322,9 @@ export default function Profile() {
                                             <button type="submit" className="save-btn-add">Save Changes <i className="bi bi-arrow-right"></i></button>
                                         </div>
                                     </form>
-
                                 </div>
-
                             </div>
-
-
                         </div>
-
-
-
-
-
                     </div>
                 </div>
             </div>
