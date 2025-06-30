876cee1a9a733ebe6f971a29637469bb538624e2
erdevkumar7 <erdevkumar7@gmail.com>
2025-06-30 13:50:47 +0530

    coach service-packages

--- src/app/coach/service-packages/page.jsx
+++ src/app/coach/service-packages/page.jsx
@@ -26,6 +26,80 @@ export default function CoachServicePackages() {
         <div className="main-panel">
             <div className="content-wrapper">
 
+                <div className="session-wrapper">
+                    <div className="session-card">
+                        <img src={`/coachsparkle/images/package1.png`} alt="Team Image" className="top-image" />
+                        <div className="session-content">
+                            <h2>Confidence Jumpstart Session</h2>
+                            <div className="icons-row">
+                                📍 Online &nbsp; | &nbsp;
+                                👤 1-on-1 coaching &nbsp; | &nbsp;
+                                📅 Jun - Aug 2025
+                            </div>
+                            <div className="icons-row">
+                                🗓️ 4 Sessions &nbsp; | &nbsp;
+                                ⏱️ 60 Min/Session
+                            </div>
+                            <div className="icons-row">
+                                🧠 Confidence, Goal Clarity, Custom Action Plan
+                            </div>
+                            <p className="session-description">
+                                A one-time deep-dive session to assess your confidence blocks, set clear goals, and walk away with a custom action plan.
+                            </p>
+                            <div className="price">$290 / Package</div>
+                            <a href="#" className="book-btn">View Details</a>
+                        </div>
+                    </div>
+
+                    <div className="session-card">
+                        <img src={`/coachsparkle/images/package2.png`} alt="Team Image" className="top-image" />
+                        <div className="session-content">
+                            <h2>Breakthrough Package</h2>
+                            <div className="icons-row">
+                                📍 Online &nbsp; | &nbsp;
+                                👤 1-on-1 coaching &nbsp; | &nbsp;
+                                📅 Jun - Aug 2025
+                            </div>
+                            <div className="icons-row">
+                                🗓️ 4 Sessions &nbsp; | &nbsp;
+                                ⏱️ 60 Min/Session
+                            </div>
+                            <div className="icons-row">
+                                🧠 Confidence, Goal Clarity, Custom Action Plan
+                            </div>
+                            <p className="session-description">
+                                Designed for short-term goal clarity and mindset shifts. Ideal if you’re preparing for a presentation, job interview, or important life transition.
+                            </p>
+                            <div className="price">$250 / Package</div>
+                            <a href="#" className="book-btn">View Details</a>
+                        </div>
+                    </div>
+
+                    <div className="session-card">
+                        <img src={`/coachsparkle/images/package2.png`} alt="Team Image" className="top-image" />
+                        <div className="session-content">
+                            <h2>Breakthrough Package</h2>
+                            <div className="icons-row">
+                                📍 Online &nbsp; | &nbsp;
+                                👤 1-on-1 coaching &nbsp; | &nbsp;
+                                📅 Jun - Aug 2025
+                            </div>
+                            <div className="icons-row">
+                                🗓️ 4 Sessions &nbsp; | &nbsp;
+                                ⏱️ 60 Min/Session
+                            </div>
+                            <div className="icons-row">
+                                🧠 Confidence, Goal Clarity, Custom Action Plan
+                            </div>
+                            <p className="session-description">
+                                Designed for short-term goal clarity and mindset shifts. Ideal if you’re preparing for a presentation, job interview, or important life transition.
+                            </p>
+                            <div className="price">$250 / Package</div>
+                            <a href="#" className="book-btn">View Details</a>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="profile-form-add">
                     <div className="card">
                         <div className="profile-form">
@@ -134,11 +208,11 @@ export default function CoachServicePackages() {
                                     </div>
 
                                     <div className='form-group col-md-4'>
-                                        <label htmlFor='session_count'> Delivey Data</label>
+                                        <label htmlFor='session_duration'> Duration Per Session</label>
                                         <select
-                                            id='session_count'
-                                            name='session_count'
-                                            value={formData.session_count}
+                                            id='session_duration'
+                                            name='session_duration'
+                                            value={formData.session_duration}
                                             onChange={handleChange}
                                         >
                                             <option value="">Select </option>
@@ -147,11 +221,11 @@ export default function CoachServicePackages() {
                                     </div>
 
                                     <div className='form-group col-md-4'>
-                                        <label htmlFor='session_count'> Session Count</label>
+                                        <label htmlFor='session_formate'> Session Formate</label>
                                         <select
-                                            id='session_count'
-                                            name='session_count'
-                                            value={formData.session_count}
+                                            id='session_formate'
+                                            name='session_formate'
+                                            value={formData.session_formate}
                                             onChange={handleChange}
                                         >
                                             <option value="">Select </option>
@@ -186,16 +260,93 @@ export default function CoachServicePackages() {
                                     </div>
 
                                     <div className='form-group col-md-4'>
-                                        <label htmlFor='currency'> Currency</label>
+                                        <label htmlFor='pricing_model'> Pricing Model</label>
                                         <select
-                                            id='currency'
-                                            name='currency'
-                                            value={formData.currency}
+                                            id='pricing_model'
+                                            name='pricing_model'
+                                            value={formData.pricing_model}
                                             onChange={handleChange}
                                         >
                                             <option value="">Select </option>
                                             <option value={1}>1</option>
                                         </select>
+                                    </div>
+                                </div>
+
+                                <div className="coach-slot-and-availability gap-4">
+                                    <div className='form-group col-md-4'>
+                                        <label htmlFor='slot_availability'> Slots Available for Booking</label>
+                                        <select
+                                            id='slot_availability'
+                                            name='slot_availability'
+                                            value={formData.slot_availability}
+                                            onChange={handleChange}
+                                        >
+                                            <option value="">Select </option>
+                                            <option value={1}>1</option>
+                                        </select>
+                                    </div>
+
+                                    <div className='form-group col-md-4'>
+                                        <label htmlFor='booking_slot'>Availablity</label>
+                                        <select
+                                            id='booking_slot'
+                                            name='booking_slot'
+                                            value={formData.booking_slot}
+                                            onChange={handleChange}
+                                        >
+                                            <option value="">Select </option>
+                                            <option value={1}>1</option>
+                                        </select>
+                                    </div>
+
+                                    <div className='form-group col-md-4'>
+                                        <label htmlFor='booking_window'>Booking Window</label>
+                                        <select
+                                            id='booking_window'
+                                            name='booking_window'
+                                            value={formData.booking_window}
+                                            onChange={handleChange}
+                                        >
+                                            <option value="">Select </option>
+                                            <option value={1}>1</option>
+                                        </select>
+                                    </div>
+                                </div>
+
+
+                                <div className="validity-cancel-resedule gap-4">
+                                    <div className='form-group col-md-4'>
+                                        <label htmlFor="session_validity">Validity</label>
+                                        <input
+                                            required
+                                            type="text"
+                                            id="session_validity"
+                                            name="session_validity"
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+
+                                    <div className='form-group col-md-4'>
+                                        <label htmlFor="cancellation_policy">Cancellation Policy</label>
+                                        <input
+                                            required
+                                            type="text"
+                                            id="cancellation_policy"
+                                            name="cancellation_policy"
+                                            onChange={handleChange}
+                                        />
+                                    </div>
+
+                                    <div className='form-group col-md-4'>
+                                        <label htmlFor="rescheduling_policy">Rescheduling Policy</label>
+                                        <input
+                                            required
+                                            type="text"
+                                            id="rescheduling_policy"
+                                            name="rescheduling_policy"
+                                            onChange={handleChange}
+                                        />
                                     </div>
                                 </div>
 
