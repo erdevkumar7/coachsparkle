9e0af22d7cced9d68551720e64fcd60491260234
Amit Patel <votiveamit.web@gmail.com>
2025-09-17 17:06:52 +0530

    implement subscription plan api

--- src/app/(guest)/_components/HomeComp/CoachPlans.jsx
+++ src/app/(guest)/_components/HomeComp/CoachPlans.jsx
@@ -1,4 +1,25 @@
+"use client";
+import axios from "axios"
+import Link from "next/link";
+import { useEffect, useState } from "react"
+
 export default function CoachPlans() {
+    const apiUrl = process.env.NEXT_PUBLIC_API_URL;
+    const [plans, setPlans] = useState([]);
+    const [planType, setPlanType] = useState("monthly");
+
+    useEffect(() => {
+        fethPlans(planType);
+    }, [planType]);
+
+    const fethPlans = async (type) => {
+        try {
+            const response = await axios.get(`${apiUrl}/subscriptionplansbyduration?type=${type}`);
+            setPlans(response.data.plans);
+        } catch (error) {
+            console.error("Error fetching plans", error);
+        }
+    };
 
 
     return (
@@ -15,73 +36,67 @@ export default function CoachPlans() {
                 <div className="row">
                     <div className="toggle-container">
                         <div className="switch-toggle">
-                            <input type="radio" name="plan" id="monthly" ></input>
-                            <input type="radio" name="plan" id="yearly"></input>
+                            <input
+                                type="radio"
+                                name="plan"
+                                id="monthly"
+                                checked={planType === "monthly"}
+                                onChange={() => setPlanType("monthly")}
+                            />
+                            <input
+                                type="radio"
+                                name="plan"
+                                id="yearly"
+                                checked={planType === "yearly"}
+                                onChange={() => setPlanType("yearly")}
+                            />
                             <label htmlFor="monthly">Monthly</label>
                             <label htmlFor="yearly">Yearly</label>
                             <div className="slider"></div>
                         </div>
                     </div>
                     <div className="pricing">
-                        <div className="col-md-4">
-                            <div className="card">
-                                <h3>Basic Plan</h3>
-                                <p>Get started with a basic profile to explore the platform and connect with your first few clients.</p>
-                                <h2>$<span className="number-add">0</span></h2>
-                                <div className="user-list-plan">
-                                    <ul>
-                                        <li><i className="bi bi-check"></i>Basic Listing In 1 Category</li>
-                                        <li><i className="bi bi-check"></i>500 Character Bio + Photo</li>
-                                        <li><i className="bi bi-check"></i> Standard AI Matching</li>
-                                        <li><i className="bi bi-check"></i>Up to  5 Notifications / Month</li>
-                                        <li><i className="bi bi-check"></i>Manual Booking Only</li>
-                                        <li><i className="bi bi-check"></i>Fixed Pricing</li>
-                                        <li><i className="bi bi-check"></i>Text Message Only</li>
-                                        <li><i className="bi bi-check"></i>Standard Support, Limited Analytics</li>
-                                    </ul>
-                                    <button>Sign up</button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-md-4">
-                            <div className="card featured">
-                                <h3>Pro Coach Plan</h3>
-                                <p>Maximize your reach and revenue with advanced tools, full visibility and priority matching</p>
-                                <h2>$<span className="number-add">190</span></h2>
-                                {/* <span className="save">Save <span> $50 </span> a year</span> */}
-                                <div className="user-list-plan pro-coach-plan-add">
-                                    <ul>
-                                        <li><i className="bi bi-check"></i>Featured Listing In Unlimited Categories With Priority</li>
-                                        <li><i className="bi bi-check"></i>3000 Character, Intro Video & Media Gallery</li>
-                                        <li><i className="bi bi-check"></i>Real Time Alerts + Priority AI Matching</li>
-                                        <li><i className="bi bi-check"></i>Unlimited Coaching Request Notifications</li>
-                                        <li><i className="bi bi-check"></i>Smart Calendar Sync + Auto Booking Capabilities</li>
-                                        <li><i className="bi bi-check"></i>Custom Pricing, Bundled Packages, and Flexible Offers</li>
-                                        <li><i className="bi bi-check"></i>Email, Video Call and Client Interest Insights</li>
-                                        <li><i className="bi bi-check"></i>24 Hours Priority Support + Full Analytics Dashboard</li>
-                                    </ul>
-                                    <button>Start Free! First 3 months on us!</button>
-                                </div>
-                            </div>
-                        </div>
+                        {plans.length > 0 ? (
+                            plans.map((plan) => (
+                                <div className="col-md-4 mb-4" key={plan.id}>
+                                    <div className={`card ${plan.plan_amount > 0 ? "featured" : ""}`}>
+                                        <h3>{plan.plan_name}</h3>
+                                        <p>{plan.plan_amount > 0 ? "Maximize your reach and revenue with advanced tools, full visibility and priority matching" : "Get started with a basic profile to explore the platform and connect with your first few clients."}</p>
+                                        <h2>$<span className="number-add">{plan.plan_amount}</span></h2>
 
-                        {/* <div className="col-md-4">
-                <div className="card">
-                  <h3>Premium</h3>
-                  <p>Lorem ipsum dolor sit amet, consectetur</p>
-                  <h2>$<span className="number-add">16</span></h2>
-                  <div className="user-list-plan">
-                    <ul>
-                      <li><i className="bi bi-check"></i>Features</li>
-                      <li><i className="bi bi-check"></i>Features</li>
-                      <li><i className="bi bi-check"></i> Features</li>
-                      <li><i className="bi bi-check"></i>Features</li>
-                      <li><i className="bi bi-check"></i>Features</li>
-                    </ul>
-                    <button>Signup</button>
-                  </div>
-                </div>
-              </div> */}
+                                        <div className="user-list-plan">
+                                            <ul>
+                                                {plan.plan_content
+                                                    ?.split(".")
+                                                    .filter((item) => item.trim() !== "")
+                                                    .map((feature, index) => {
+                                                        const cleanFeature = feature.replace(/<[^>]+>/g, "").trim();
+                                                        return (
+                                                            <li key={index}>
+                                                                <i className="bi bi-check"></i> {cleanFeature}
+                                                            </li>
+                                                        );
+                                                    })
+                                                }
+                                            </ul>
+                                            {plan.plan_amount > 0 ? (
+                                                <button>
+                                                    Subscribe
+                                                </button>
+                                            ) : (
+                                                <Link href="/select-role">
+                                                    <button>
+                                                        Sign up
+                                                    </button>
+                                                </Link>
+                                            )}
+                                        </div>
+                                    </div>
+                                </div>
+                            ))
+                        ) : (
+                            <p className="text-center">No {planType} plans available.</p>
+                        )}
                     </div>
                 </div>
             </div>
